a42b5dc886848fc2a4cf0ab0c111f48b42fed9a0
hareb <hareb.div@gmail.com>
2024-09-26 09:52:56 +0400

    fix popup after sign up

--- src/components/auth/RegisterPage.js
+++ src/components/auth/RegisterPage.js
@@ -230,7 +230,7 @@ export default function RegisterPageComponent() {
 
               // </p>
               <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 m-0">
-                <div className="p-6 max-w-md w-full mx-4">
+                <div className="bg-white dark:bg-zinc-800 rounded-lg shadow-xl p-6 max-w-md w-full mx-4">
                   <p className="text-gray-600 dark:text-gray-300 mb-6">
                     {openModal}
                   </p>
