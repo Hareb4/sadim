9bdef08d3811e4c8a262922d3bf0d092dd597aba
hareb <hareb.div@gmail.com>
2024-11-13 11:34:54 +0400

    fix things

--- src/app/notes/layout.js
+++ src/app/notes/layout.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect, useMemo } from "react";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import Sidebar from "@/components/layout/Sidebar";
 import { useTheme } from "next-themes";
+import { NotesProvider } from "@/context/NotesContext";
 
 const ibmarabic = IBM_Plex_Sans_Arabic({
   subsets: ["arabic"],
@@ -66,21 +67,23 @@ export default function Layout({ children }) {
 
   return (
     <ProtectedRoute>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <div
-          className={`h-screen flex flex-col sm:flex-row-reverse ${ibmarabic.className}`}
-        >
-          {/* Sidebar */}
-          {isSidebarVisible && <Sidebar />}
+      <NotesProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <div
+            className={`h-screen flex flex-col sm:flex-row-reverse ${ibmarabic.className}`}
+          >
+            {/* Sidebar */}
+            {isSidebarVisible && <Sidebar />}
 
-          {/* Main Content */}
-          <div className="flex-1 overflow-hidden hide-scrollbar">
-            <main className="h-full overflow-y-auto overflow-x-hidden hide-scrollbar">
-              {children}
-            </main>
+            {/* Main Content */}
+            <div className="flex-1 overflow-hidden hide-scrollbar">
+              <main className="h-full overflow-y-auto overflow-x-hidden hide-scrollbar">
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </NotesProvider>
     </ProtectedRoute>
   );
 }
--- src/components/customeui/NoteSkeleton.jsx
+++ src/components/customeui/NoteSkeleton.jsx
@@ -1,7 +1,7 @@
 // components/NoteSkeleton.jsx
 export default function NoteSkeleton() {
   return (
-    <div className="animate-pulse">
+    <div className="animate-pulse" dir="rtl">
       <div className="h-64 bg-gray-200 dark:bg-gray-700 mb-4"></div>
       <div className="h-12 bg-gray-200 dark:bg-gray-700 mb-4 w-3/4"></div>
       <div className="h-4 bg-gray-200 dark:bg-gray-700 mb-2 w-1/2"></div>
